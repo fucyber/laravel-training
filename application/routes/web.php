ae9c9c3f51072a5a96f4a04409ad13eb6855de20
sarit.w <sarit.w@touchtechnologies.co.th>
2020-07-31 09:41:28 +0700

    source code

--- application/routes/web.php
+++ application/routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+ 
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $databaseName = \DB::table('news')->get();
+    dd($databaseName);
+
+
+    return response()->json([
+            'success' => true,  
+            'data' =>  'Hello world'
+        ], 200);
 });
